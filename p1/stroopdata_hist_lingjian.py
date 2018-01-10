6460179e8284dec769af2af4ebbb9f9491d09a3b
tynbl <tynbl@sohu.com>
2018-01-10 17:38:58 +0800

    20180110

---
+++ p1/stroopdata_hist_lingjian.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# 设置绘图样式
+plt.style.use('seaborn-colorblind')
+
+# 读取数据
+df = pd.read_csv('stroopdata.csv')
+
+# 绘制直方图
+df.plot(kind='hist', alpha=0.5).get_figure().savefig('stroopdata_hist.png')
